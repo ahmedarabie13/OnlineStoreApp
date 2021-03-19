2cafbbf87f4ab9c8fec8499546fc624421952656
Ahmed <engarabie13@gmail.com>
2021-03-19 18:33:33 +0200

    addinf productEntity

---
+++ src/main/java/gov/iti/jets/team5/models/entity/ProductEntity.java
@@ -0,0 +1,9 @@
+package gov.iti.jets.team5.models.entity;
+
+public class ProductEntity {
+    private int productId;
+    private String name;
+    private String category;
+    private String description;
+
+}
