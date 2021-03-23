cd0114011eda917904688575c491cf3ef752caf8
MahaMekdad <maha_mekdad@outlook.com>
2021-03-23 23:40:46 +0200

    trying out the mapping

--- src/main/java/gov/iti/jets/team5/repositories/UserRepository.java
+++ src/main/java/gov/iti/jets/team5/repositories/UserRepository.java
@@ -1,4 +1,25 @@
 package gov.iti.jets.team5.repositories;
 
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+
+import java.util.List;
+
 public class UserRepository {
+
+    //todo session will be given in a different way maybe?
+    public static boolean isRegistered(String enteredEmail, Session session){
+        session.beginTransaction();
+        Query q = session.createQuery("select u.email from UserData u");
+        List<String> emails = q.list();
+        boolean flag = false;
+        for (String e : emails) {
+            System.out.println(e + " <----- DB Email");
+            if(e.equals(enteredEmail)){
+                flag = true;
+                break;
+            }
+        }
+        return flag;
+    }
 }
--- src/main/java/gov/iti/jets/team5/services/impl/RegisterServiceImpl.java
+++ src/main/java/gov/iti/jets/team5/services/impl/RegisterServiceImpl.java
@@ -1,35 +1,58 @@
 package gov.iti.jets.team5.services.impl;
 
 import gov.iti.jets.team5.models.dto.UserDto;
+import gov.iti.jets.team5.repositories.UserRepository;
 import gov.iti.jets.team5.services.RegisterService;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 public class RegisterServiceImpl implements RegisterService {
 
     private static RegisterServiceImpl registerServiceInstance = null;
 
+    //todo maybe moved to a class that generates sessions for the services classes
+    StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
+    SessionFactory sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+    Session session = sessionFactory.openSession();
+
     public static RegisterService getInstance() {
+        System.out.println("inside3 get instance");
         if (registerServiceInstance == null) {
+            System.out.println("inside4 get instance");
             synchronized (RegisterServiceImpl.class) {
                 if (registerServiceInstance == null) {
+                    System.out.println("inside5 get instance");
                     registerServiceInstance = new RegisterServiceImpl();
                 }
+                System.out.println("inside6 get instance");
             }
+            System.out.println("inside7 get instance");
         }
+        System.out.println("inside8 get instance");
         return registerServiceInstance;
     }
 
     @Override
-    public Boolean isRegistered(String email) {
+    public boolean isRegistered(String email) {
+        //boolean isRegistered = UserRepository.isRegistered(email, session);
         //todo real db check
         if (email.equals("maha@gmail.com")) {
             return true;
         } else {
             return false;
         }
+//        if (isRegistered) {
+//            return true;
+//        } else {
+//            return false;
+//        }
     }
 
     @Override
-    public Boolean registerUser(UserDto userDto) {
+    public boolean registerUser(UserDto userDto) {
         //todo go and register the user in the db
         //todo either to send the object itself or the attributes in it? according to hibernate
         return true;
