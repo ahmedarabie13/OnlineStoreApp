c249d93844468f59327fc52b4b9d8ae2c3c915e8
Ahmed <engarabie13@gmail.com>
2021-03-25 23:25:10 +0200

    initiate cart Service

--- src/main/java/gov/iti/jets/team5/controllers/CartServlet.java
+++ src/main/java/gov/iti/jets/team5/controllers/CartServlet.java
@@ -7,10 +7,15 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-@WebServlet(name = "cart",urlPatterns = "/cart")
+
+@WebServlet(name = "cart", urlPatterns = "/cart")
 public class CartServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("cart.jsp").forward(request,response);
+        request.getRequestDispatcher("cart.jsp").forward(request, response);
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
     }
 }
